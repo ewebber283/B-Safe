d67fe2ba2ed3c79998914d4a397dfe6f698a497d
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-26 21:14:04 -0500

    fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 fetch(
-    'https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/1/2020-09-01/2020-12-01'
-  )
+    'https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-09-01/2020-12-01'
+)
     .then(function(response) {
-      return response.json();
+    return response.json();
     })
     .then(function(data) {
-      console.log(data);
+    console.log(data);
     });
