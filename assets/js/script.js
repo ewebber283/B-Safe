5902458e30b81bafc67f19aea1c95d9c9d0c9f3e
Kateryna B <butsneva.ekaterina@gmail.com>
2021-05-05 10:30:49 -0500

    add ccovid details

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,28 +11,89 @@
     https://covid-19.dataflowkit.com/v1 */
 
 
+
+
+
 var searchBtn = document.querySelector('#search-btn')
 searchBtn.addEventListener('click', searchOnClick)
 
 function searchOnClick() {
-    var usersInput = document.getElementById('search-term').value
-    console.log(usersInput)
-
-    fetch(
-        'https://covid-19.dataflowkit.com/v1/' + usersInput
-        )
-            .then(function(response) {
-            return response.json();
-            })
-            .then(function(data) {
-            console.log(data);
-            // use this format to search for the country: Italy ; Spain ; USA ; Mexico
-            // all lower case letters will give you an error. (this will be fixed later)
-            if (usersInput !==  data.Country_text) {
-                alert('Please enter a valid country name.')
-            } else {
-            var countrySearchTerm = document.querySelector('#country-search-term')
-                countrySearchTerm.textContent = data['Active Cases_text']
-            }});
+    var userInput = upperFirst(document.getElementById('search-term').value)
+    console.log(userInput)  
+    apiRequests(userInput)  
 }
 
+function apiRequests(userInput) {
+
+    // fetching active/total/new cases info
+    fetch(
+        'https://covid-19.dataflowkit.com/v1/' + userInput
+        )
+            .then(function(response) {
+            return (response.json())
+            })
+            .then(function(cases) {
+            if (userInput !==  cases.Country_text) {
+                alert('Please enter a valid country name.')
+            } else {
+                console.log(cases)
+                printCaseData(cases)
+            }}
+        )
+
+    // fetching vaccines info
+    fetch (
+        'https://covid-api.mmediagroup.fr/v1/vaccines?country=' + userInput
+        )
+            .then(function(response) {
+            return response.json()
+            })
+            .then(function(vaccines) {
+            console.log(vaccines)
+            printVaccineData(vaccines)
+            })
+
+            
+}
+
+
+function upperFirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+
+function printCaseData (country) {
+    var reportSecction = document.querySelector('#report-section')
+        reportSecction.innerHTML = ''
+
+    var reportEl = $('<div>').addClass('numbers')
+        if (country["Active Cases_text"] === '') {
+            var noDataActive = ('<p id="active-cases">Active Cases: no data</p>')
+        } else {
+            var activeCases = ('<p id="active-cases">Active Cases: ' + country["Active Cases_text"] + '</p>')
+        }
+
+        if(country["New Cases_text"] === ''){
+            var noDataNew = ('<p id="active-cases">Active Cases: no data</p>')
+        } else {
+            var newCases = ('<p id="new-cases">New Cases: ' + country["New Cases_text"] + '</p>')
+        }
+
+    $('#report-section').append(reportEl)
+    reportEl.append(activeCases, newCases, noDataActive, noDataNew)
+}
+
+function printVaccineData (vaccines) {
+    if (vaccines.All.people_vaccinated === '') {
+        var noDataFully = ('<p id="vaccinated">Fully Vaccinated: no data</p>')
+    } else {
+        var fullyVaccinated = $('<p id="vaccinated">Fully Vaccinated: ' + vaccines.All.people_vaccinated + '</p>')
+    }
+
+    if (vaccines.All.people_partially_vaccinated === '') {
+        var noDataPartially = ('<p id="vaccinated">Partially Vaccinated: no data</p>')
+    } else {
+        var partiallyVaccinated = $('<p id="vaccinated">Partially Vaccinated: ' + vaccines.All.people_partially_vaccinated + '</p>')
+    }
+
+    $('#report-section').append(fullyVaccinated, partiallyVaccinated, noDataFully, noDataPartially)
+}
